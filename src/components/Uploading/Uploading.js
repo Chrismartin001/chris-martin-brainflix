e73a8b7ffc43f5452c75a81ce27e482d4fc476f1
Chris <christopher.martin5555@gmail.com>
2021-11-07 20:51:45 -0800

    Final commit fixed a few styling problems

--- src/components/Uploading/Uploading.js
+++ src/components/Uploading/Uploading.js
@@ -9,19 +9,22 @@ export default function Uploading() {
       <h2 className="upload__title">Upload Video</h2>
       <hr className="upload__hidden" />
       <h3 className="upload__thumb-title">VIDEO THUMBNAIL</h3>
-      <img className="upload__bike" src={Bike} alt="Bike rider riding a blue and black bike" />
+      <div className="container1">
+        <img className="upload__bike" src={Bike} alt="Bike rider riding a blue and black bike" />
 
-      <form className="comment1">
-        <label className="comment1__lable">TITLE YOUR VIDEO</label>
-        <input type="text" name="content" className="comment1__title" required placeholder="Add a title to your video" />
-        <label className="comment1__lable">ADD A VIDEO DESCRIPTION</label>
-        <input type="text" name="content" className="comment1__description" required placeholder="Add a description to your video" />
-        <hr className="upload__hidden" />
-        <div className="comment1__overlay">
-          <button className="comment1__button">Publish</button>
-          <h2 className="comment1__cancel">CANCEL</h2>
-        </div>
-      </form>
+        <section className="comment1">
+          <label className="comment1__lable">TITLE YOUR VIDEO</label>
+          <input type="text" name="content" className="comment1__title" required placeholder="Add a title to your video" />
+          <label className="comment1__lable">ADD A VIDEO DESCRIPTION</label>
+          <input type="text" name="content" className="comment1__description" required placeholder="Add a description to your video" />
+
+          <hr className="upload__hidden" />
+          <div className="comment1__overlay">
+            <button className="comment1__button">Publish</button>
+            <h2 className="comment1__cancel">CANCEL</h2>
+          </div>
+        </section>
+      </div>
     </article>
   );
 }
